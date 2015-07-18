605de66a0f780f926ba441fb871844596085e1f2
Евгений Губин <hemet.mail@gmail.com>
2015-07-18 17:30:23 +0300

    Update copyright notices. Bump MVVMKit.

--- DLife/Misc/DebugUtils.swift
+++ DLife/Misc/DebugUtils.swift
@@ -3,7 +3,7 @@
 //  DLife
 //
 //  Created by Евгений Губин on 16.06.15.
-//  Copyright (c) 2015 SimbirSoft. All rights reserved.
+//  Copyright (c) 2015 GitHub. All rights reserved.
 //
 
 import Foundation
--- DLifeTests/DLifeTests.swift
+++ DLifeTests/DLifeTests.swift
@@ -3,7 +3,7 @@
 //  DeclarativeUITests
 //
 //  Created by Eugene Gubin on 25.03.15.
-//  Copyright (c) 2015 SimbirSoft. All rights reserved.
+//  Copyright (c) 2015 GitHub. All rights reserved.
 //
 
 import UIKit
